15525fe792354a601f635be7cd2283d0a6b9dbf0
Ankit Pathak <ankit.pathak@acquia.com>
2023-08-01 14:57:55 +0530

    ACMS-1386: Updated the global commands for telemetry to capture install time.

--- src/Utility/AcquiaTelemetry.php
+++ src/Utility/AcquiaTelemetry.php
@@ -2,16 +2,34 @@
 
 namespace Acquia\GlobalCommands\Utility;
 
+/**
+ * Acquia telemetery class to capture site install time.
+ */
 class AcquiaTelemetry {
 
+  /**
+   * Singleton instance.
+   *
+   * @var AcquiaTelemetry
+   */
   private static $instance;
 
-  private $telemetryData = [];
+  /**
+   * An array of telemetry data.
+   *
+   * @var array
+   */
+  private array $telemetryData = [];
 
   private function __construct() {
   // Private constructor to prevent direct instantiation
   }
 
+  /**
+   * Singleton method for this acquia telemetry class.
+   *
+   * @return AcquiaTelemetry
+   */
   public static function getInstance(): AcquiaTelemetry {
     if (self::$instance === null) {
       self::$instance = new self();
@@ -19,24 +37,50 @@ class AcquiaTelemetry {
     return self::$instance;
   }
 
+  /**
+   * Sets the time of installing the site.
+   *
+   * @param string $key
+   *   Key to store the install time.
+   * @param string $time
+   *   Install time value.
+   *
+   * @return void
+   */
   public function setTime(string $key, string $time): void {
     // Store the DateTime object in the telemetry data
     $this->telemetryData[$key] = $time;
   }
 
+  /**
+   * Gets the time of installing the site.
+   *
+   * @param string $key
+   *   Key to store the install time.
+   *
+   * @return string|null
+   *   Install time of site.
+   */
   public function getTime(string $key): ?string {
     return $this->telemetryData[$key];
   }
 
-  public function getTimeDifferenceInSeconds(string $key1, string $key2): ?string {
-    // Get DateTime objects for the specified keys
-    $dateTime1 = $this->telemetryData[$key1];
-    $dateTime2 = $this->telemetryData[$key2];
-
+  /**
+   * Prepare time duration in seconds.
+   *
+   * @param string $start
+   *   Start time.
+   * @param string $end
+   *   End time.
+   *
+   * @return string|null
+   *   Time format in seconds.
+   */
+  public function getTimeDifferenceInSeconds(string $start, string $end): ?string {
     // Calculate the time difference in seconds
-    $timeDifference = $dateTime2 - $dateTime1;
+    $timeDifference = $this->telemetryData[$end] - $this->telemetryData[$start];
 
-    return number_format($timeDifference, 2);;
+    return number_format($timeDifference, 2);
   }
 
 }
