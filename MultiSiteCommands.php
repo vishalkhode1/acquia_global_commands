ee193170b614a681a77694179e5971ad97369856
“Chandan <chandan.singh@acquia.com>
2023-09-29 19:23:26 +0530

    ACMS-1981: Add logic to setup multisite.

--- MultiSiteCommands.php
+++ MultiSiteCommands.php
@@ -4,6 +4,9 @@ namespace Drush\Commands\acquia_global_commands;
 
 use Consolidation\AnnotatedCommand\CommandData;
 use Drush\Commands\DrushCommands;
+use Robo\Contract\BuilderAwareInterface;
+use Robo\Contract\VerbosityThresholdInterface;
+use Robo\LoadAllTasks;
 use Symfony\Component\Console\Question\ConfirmationQuestion;
 use Symfony\Component\Console\Question\Question;
 
@@ -12,7 +15,9 @@ use Symfony\Component\Console\Question\Question;
  *
  * @package Drupal\acquia_global_commands\Commands
  */
-class MultiSiteCommands extends DrushCommands {
+class MultiSiteCommands extends DrushCommands implements BuilderAwareInterface {
+
+  use LoadAllTasks;
 
   /**
    * Execute code before site:install command.
@@ -21,12 +26,28 @@ class MultiSiteCommands extends DrushCommands {
    *   The information about the current request.
    *
    * @hook pre-command site:install
+   * @throws \Exception
    */
   public function preSiteInstallCommand(CommandData $commandData): void {
     $options = $commandData->options();
     $uri = $options['uri'] ?? "";
     $dbUrl = $options['db-url'] ?? "";
     $existingConfig = $options['existing-config'] ?? FALSE;
+
+    if ($uri) {
+      $site_name = $this->getNewSiteName($uri);
+      $new_site_dir = $this->getConfigValue('docroot') . '/sites/' . $site_name;
+
+      if (file_exists($new_site_dir)) {
+        throw new \Exception("Cannot generate new multisite, $new_site_dir already exists!");
+      }
+
+      $this->say("This will generate a new site in the docroot/sites/$site_name directory.");
+      $default_site_dir = $this->getConfigValue('docroot') . '/sites/default';
+      $this->createNewSiteDir($default_site_dir, $new_site_dir);
+      $this->createNewSiteConfigDir($site_name);
+    }
+
     if ($uri && !$dbUrl && !$existingConfig) {
       $question = new ConfirmationQuestion("Would you like to configure the local database credentials?", TRUE);
       $answer = $this->io()->askQuestion($question);
@@ -50,7 +71,67 @@ class MultiSiteCommands extends DrushCommands {
         $commandData->input()->setOption("db-url", "mysql://$dbUser:$dbPassword@$dbHost:$dbPort/$dbName");
       }
     }
-
+  }
+  /**
+   * Get new site name.
+   *
+   * @param string $uri
+   *   Options.
+   *
+   * @return string
+   *   Site name.
+   */
+  private function getNewSiteName(string $uri) {
+    $site_name = parse_url($uri);
+    if (!empty($site_name)){
+      if (isset($site_name['scheme'])) {
+        $site_name = $site_name['host'];
+      }
+      else {
+        $site_name = $site_name['path'];
+      }
+    }
+    // @todo get site name from $sites value.
+    return $site_name;
   }
 
+  /**
+   * Create new site dir.
+   *
+   * @param string $default_site_dir
+   *   Default site dir.
+   * @param string $new_site_dir
+   *   New site dir.
+   *
+   * @throws \Exception
+   */
+  protected function createNewSiteDir(string $default_site_dir, string $new_site_dir) {
+    $result = $this->taskCopyDir([
+      $default_site_dir => $new_site_dir,
+    ])
+      ->exclude(['local.settings.php', 'files'])
+      ->setVerbosityThreshold(VerbosityThresholdInterface::VERBOSITY_VERBOSE)
+      ->run();
+    if (!$result->wasSuccessful()) {
+      throw new \Exception("Unable to create $new_site_dir.");
+    }
+  }
+
+  /**
+   * Create new config dir.
+   *
+   * @param string $site_name
+   *   Site name.
+   * @throws \Exception
+   */
+  protected function createNewSiteConfigDir(string $site_name) {
+    $config_dir = $this->getConfigValue('docroot') . '/' . $this->getConfigValue('cm.core.path') . '/' . $site_name;
+    $result = $this->taskFilesystemStack()
+      ->mkdir($config_dir)
+      ->setVerbosityThreshold(VerbosityThresholdInterface::VERBOSITY_VERBOSE)
+      ->run();
+    if (!$result->wasSuccessful()) {
+      throw new \Exception("Unable to create $config_dir.");
+    }
+  }
 }
