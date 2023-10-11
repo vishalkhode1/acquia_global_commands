b64500a9bb6e321ad7e54fdc1ccd2f90dafdfa76
“Chandan <chandan.singh@acquia.com>
2023-10-11 10:58:54 +0530

    ACMS-1981: Implement multisite suppoert with drush si command.

--- MultiSiteCommands.php
+++ MultiSiteCommands.php
@@ -2,14 +2,14 @@
 
 namespace Drush\Commands\acquia_global_commands;
 
+use Acquia\Drupal\RecommendedSettings\Config\ConfigInitializer;
+use Acquia\Drupal\RecommendedSettings\Config\SettingsConfig;
 use Consolidation\AnnotatedCommand\CommandData;
-use Drupal\Core\Site\Settings;
-use Drush\Boot\DrupalBootLevels;
 use Drush\Commands\DrushCommands;
-use Drush\Drush;
+use Drush\Sql\SqlBase;
 use Robo\Contract\BuilderAwareInterface;
-use Robo\Contract\VerbosityThresholdInterface;
 use Robo\LoadAllTasks;
+use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\Filesystem\Path;
 
 /**
@@ -22,93 +22,137 @@ class MultiSiteCommands extends DrushCommands implements BuilderAwareInterface {
   use LoadAllTasks;
 
   /**
+   * Settings warning.
+   *
+   * @var string
+   * Warning text added to the end of settings.php to point people
+   * to the Acquia Drupal Recommended Settings
+   * docs on how to include settings.
+   *
+   * @todo use these variable from DRS's Settings.php
+   */
+  private string $settingsWarning = <<<WARNING
+/**
+ * IMPORTANT.
+ *
+ * Do not include additional settings here. Instead, add them to settings
+ * included by `acquia-recommended.settings.php`. See Acquia's documentation for more detail.
+ *
+ * @link https://docs.acquia.com/
+ */
+WARNING;
+
+  /**
    * Execute code before pre-validate site:install.
    *
    * @hook pre-validate site:install
    */
-  public function validateSiteInstall(CommandData $commandData): void {
-    $siteName = $this->getSitesSubdirFromUri(DRUPAL_ROOT, $commandData->input()->getOption('uri'));
-    $this->say("This will generate a new site in the docroot/$siteName directory.");
-    $commandData->input()->setOption('sites-subdir', $siteName);
+  public function preValidateSiteInstall(CommandData $commandData): void {
+    $sitesSubdir = $this->getSitesSubdirFromUri(DRUPAL_ROOT, $commandData->input()->getOption('uri'));
+    $commandData->input()->setOption('sites-subdir', $sitesSubdir);
   }
 
   /**
-   * Execute code after site:install command.
+   * Execute code before pre-validate site:install.
    *
-   * @hook post-command site:install
-   * @param $result
-   * @param \Consolidation\AnnotatedCommand\CommandData $commandData
-   *   The drush command data.
+   * @hook validate site:install
    *
    * @throws \Exception
    */
-  public function postSiteInstallCommand($result, CommandData $commandData): void {
-    $options = $commandData->options();
-    $rootDir = $options['root'] ?? DRUPAL_ROOT;
-    $sitePath = $this->getSitePath();
-    $siteName = explode('/', $sitePath)[1];
-
-    $new_site_dir = $rootDir . '/' . $sitePath;
-
-    if ($siteName != 'default') {
-      $default_site_dir = $rootDir . '/sites/default';
-      $this->createNewSiteDir($default_site_dir, $new_site_dir);
-      $this->createNewSiteConfigDir($siteName, $rootDir);
-    }
-
+  public function validate(CommandData $commandData): void {
+    $sitesSubDir = $commandData->input()->getOption('sites-subdir');
+    $this->createSettingFiles($commandData, $sitesSubDir);
+    // @todo currently site:install command adds db credentials
+    // in settings.php instead of local.settings.php
+    // we have to figure out that issue.
   }
 
   /**
-   * Create new site dir.
+   * Create settings related files.
    *
-   * @param string $default_site_dir
-   *   Default site dir.
-   * @param string $new_site_dir
-   *   New site dir.
+   * Before pre command hook of site:install.
    *
    * @throws \Exception
    */
-  protected function createNewSiteDir(string $default_site_dir, string $new_site_dir): void {
-    $result = $this->taskCopyDir([
-      $default_site_dir => $new_site_dir,
-    ])
-      ->exclude(['local.settings.php', 'settings.php', 'files'])
-      ->setVerbosityThreshold(VerbosityThresholdInterface::VERBOSITY_VERBOSE)
-      ->run();
-    if (!$result->wasSuccessful()) {
-      throw new \Exception("Unable to create $new_site_dir.");
-    }
-  }
+  protected function createSettingFiles(CommandData $commandData, string $sitesSubDir): void {
+    $sitesSubDir = Path::join('sites', $sitesSubDir);
+    $settingsFile = Path::join($sitesSubDir, 'settings.php');
+    $localSettingsFile = Path::join($sitesSubDir . '/settings', 'local.settings.php');
+    $localSettingsPath = $sitesSubDir . '/settings';
+    $fileSystem = new Filesystem();
 
-  /**
-   * Create config directory for given multisite.
-   *
-   * @param string $site_name
-   *   The site machine name.
-   * @param string $rootDir
-   *   The document root path.
-   *
-   * @throws \Exception
-   */
-  protected function createNewSiteConfigDir(string $site_name, string $rootDir): void {
-    $config_dir = $rootDir . '/' . Settings::get('config_sync_directory', 'config') . '/' . $site_name;
-    $result = $this->taskFilesystemStack()
-      ->mkdir($config_dir)
-      ->setVerbosityThreshold(VerbosityThresholdInterface::VERBOSITY_VERBOSE)
-      ->run();
-    if (!$result->wasSuccessful()) {
-      throw new \Exception("Unable to create $config_dir.");
+    if (!drush_file_not_empty($settingsFile)) {
+      // Create site sub directory.
+      if (!file_exists($sitesSubDir)) {
+        $fileSystem->mkdir($sitesSubDir);
+      }
+      // Create settings subdirectory.
+      if (!file_exists($localSettingsPath)) {
+        $this->say("This will generate a new site in the docroot/$sitesSubDir directory.");
+        $fileSystem->mkdir($localSettingsPath);
+      }
+      // Create local.settings.php out of default.local.settings.php.
+      if (!drush_op('copy', 'sites/default/settings/default.local.settings.php', $localSettingsFile)) {
+        throw new \Exception(dt('Failed to copy sites/default/settings/default.local.settings.php to @settingsFile', ['@settingsFile' => $localSettingsFile]));
+      }
+      // Create settings.php out of default.settings.php.
+      if (!drush_op('copy', 'sites/default/default.settings.php', $settingsFile)) {
+        throw new \Exception(dt('Failed to copy sites/default/default.settings.php to @settingsfile', ['@settingsfile' => $settingsFile]));
+      }
+      $this->updateDbSpec($commandData, $sitesSubDir, $localSettingsFile);
+      // @todo use these function from DRS's Settings.php
+      $this->appendIfMatchesCollect($settingsFile, '#vendor/acquia/drupal-recommended-settings/settings/acquia-recommended.settings.php#', 'require DRUPAL_ROOT . "/../vendor/acquia/drupal-recommended-settings/settings/acquia-recommended.settings.php";' . "\n");
+      $this->appendIfMatchesCollect($settingsFile, '#Do not include additional settings here#', $this->settingsWarning . "\n");
     }
   }
 
   /**
-   * Get site path string.
+   * Update database specification.
    *
    * @throws \Exception
    */
-  protected function getSitePath(): \UnitEnum|float|array|bool|int|string|null {
-    Drush::bootstrapManager()->doBootstrap(DrupalBootLevels::FULL);
-    return \Drupal::getContainer()->getParameter('site.path');
+  protected function updateDbSpec(CommandData $commandData, string $sitesSubDir, string $localSettingsFile): void {
+    $root = $commandData->input()->getOption('root');
+    $configInitializer = new ConfigInitializer($root, $root, '');
+    $configInitializer->setSite($sitesSubDir);
+    if ($sql = SqlBase::create($commandData->input()->getOptions())) {
+      $db_spec = $sql->getDbSpec();
+      // @todo create addDbConfig function in DRS's ConfigInitializer.php
+      $config = $configInitializer->addDbConfig([
+        'drupal' => [
+          'db' => [
+            'database' => $db_spec['database'],
+            'username' => $db_spec['username'],
+            'password' => $db_spec['password'],
+            'host' => $db_spec['host'],
+            'port' => $db_spec['port'],
+          ],
+        ],
+      ]);
+      $settings = new SettingsConfig($config->export());
+      $settings->expandFileProperties($localSettingsFile);
+    }
+  }
+
+  /**
+   * Append the string to file, if matches.
+   *
+   * @param string $file
+   *   The path to file.
+   * @param string $pattern
+   *   The regex patten.
+   * @param string $text
+   *   Text to append.
+   * @param bool $shouldMatch
+   *   Decides when to append if match found.
+   */
+  protected function appendIfMatchesCollect(string $file, string $pattern, string $text, bool $shouldMatch = FALSE): void {
+    // @todo use these function from DRS's Settings.php
+    $contents = file_get_contents($file);
+    if (preg_match($pattern, $contents) == $shouldMatch) {
+      $contents .= $text;
+    }
+    (new Filesystem())->dumpFile($file, $contents);
   }
 
   /**
@@ -116,8 +160,10 @@ class MultiSiteCommands extends DrushCommands implements BuilderAwareInterface {
    * provided uri.
    *
    * This code copied from SiteInstallCommands.php file.
+   *
+   * @return array|false|mixed|string|string[]
    */
-  protected function getSitesSubdirFromUri($root, $uri) {
+  private function getSitesSubdirFromUri($root, $uri) {
     $dir = strtolower($uri);
     // Always accept simple uris (e.g. 'dev', 'stage', etc.)
     if (preg_match('#^[a-z0-9_-]*$#', $dir)) {
@@ -129,7 +175,7 @@ class MultiSiteCommands extends DrushCommands implements BuilderAwareInterface {
     if ($dir && file_exists(Path::join($root, $dir))) {
       return $dir;
     }
-    // Find the dir from sites.php file
+    // Find the dir from sites.php file.
     $sites_file = $root . '/sites/sites.php';
     if (file_exists($sites_file)) {
       $sites = [];
@@ -142,7 +188,7 @@ class MultiSiteCommands extends DrushCommands implements BuilderAwareInterface {
     if (file_exists(Path::join($root, 'sites', 'default'))) {
       return 'default';
     }
-    return false;
+    return FALSE;
   }
 
 }
